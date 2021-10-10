f3de1fb237ee72969d338b68c63634e89c3b66ee
ardagoktas-7 <ardagoktas2000@gmail.com>
2021-10-10 15:52:43 +0300

    try1

---
+++ documents/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Test,Document
+# Register your models here.
+
+admin.site.register(Test)
+admin.site.register(Document)
---
+++ documents/cron.py
@@ -0,0 +1,14 @@
+from .models import Test, Document
+from datetime import datetime
+
+def my_scheduled_job():
+  Test.objects.create(name='test')
+
+def documents_expired_check():
+    today = datetime.today().strftime('%Y-%m-%d')
+    qs = Document.objects.filter(expired=False)
+    for doc in qs:
+        exp = doc.expiration_date.strftime('%Y-%m-%d')
+        if exp < today:
+            doc.expired = True
+            doc.save()
